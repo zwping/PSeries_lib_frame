dbaec5a1c70bb4a6bc4523fb770f76406f28f084
zwping <1101558280@qq.com>
2019-05-07 09:38:26 +0800

    debug ing

---
+++ src/main/java/win/zwping/frame/HawkUtil.java
@@ -0,0 +1,53 @@
+package win.zwping.frame;
+
+import android.content.Context;
+
+import com.orhanobut.hawk.Hawk;
+
+/**
+ * <p>describe：替换SharedPreferences的框架
+ * <p>    note：
+ * <p> @author：zwp on 2019-04-12 10:19:04 mail：1101558280@qq.com web: http://www.zwping.win </p>
+ */
+public class HawkUtil {
+
+    public static void init(Context context) {
+        Hawk.init(context).build();
+    }
+
+    public HawkUtil() {
+        throw new UnsupportedOperationException("u can't instantiate me..."); //不支持操作的异常
+    }
+
+    public static <T> boolean put(String key, T value) {
+        return Hawk.put(key, value);
+    }
+
+    public static String getString(String key) {
+        return Hawk.get(key, null);
+    }
+
+    public static int getInt(String key) {
+        return Hawk.get(key, -1);
+    }
+
+    public static long getLong(String key) {
+        return Hawk.get(key, 0L);
+    }
+
+    public static Boolean getBoolean(String key) {
+        return Hawk.get(key, false);
+    }
+
+    public static <T> T get(String key) {
+        return Hawk.get(key);
+    }
+
+    public static void delete(String key) {
+        Hawk.delete(key);
+    }
+
+    public static void deleteAll() {
+        Hawk.deleteAll();
+    }
+}
---
+++ src/main/java/win/zwping/frame/comm/CommPop.java
@@ -0,0 +1,185 @@
+package win.zwping.frame.comm;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.Nullable;
+
+import razerdp.basepopup.BasePopupWindow;
+import win.zwping.code.review.PEditText;
+import win.zwping.code.review.PImageView;
+import win.zwping.code.review.PTextView;
+import win.zwping.code.utils.ScreenUtil;
+import win.zwping.frame.R;
+
+import static android.view.View.GONE;
+
+/**
+ * <p>describe：
+ * <p>    note：
+ * <p> @author：zwp on 2019-05-06 16:50:17 mail：1101558280@qq.com web: http://www.zwping.win </p>
+ */
+public class CommPop extends BasePopupWindow {
+
+    protected PImageView exitPiv;
+    protected PEditText contentPet;
+    protected PTextView contentPtv, cancelPtv, confirmPtv;
+    protected View line, hLine;
+
+    private boolean autoCancel = true;
+
+    public CommPop(Context context) {
+        super(context, (int) (ScreenUtil.getScreenWidth() * 0.75), ViewGroup.LayoutParams.WRAP_CONTENT);
+        exitPiv = findViewById(R.id.exit_piv);
+        contentPet = findViewById(R.id.content_pet);
+        contentPtv = findViewById(R.id.content_ptv);
+        cancelPtv = findViewById(R.id.cancel_ptv);
+        confirmPtv = findViewById(R.id.confirm_ptv);
+        line = findViewById(R.id.line_v);
+        hLine = findViewById(R.id.h_line_v);
+        exitPiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        cancelPtv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoCancel) dismiss();
+            }
+        });
+
+        setAllowDismissWhenTouchOutside(false);
+    }
+
+
+    @Override
+    public View onCreateContentView() {
+        return View.inflate(getContext(), R.layout.comm_pop, null);
+    }
+
+    public CommPop setIsInput(Boolean input) {
+        contentPet.setVisibility(input ? View.VISIBLE : GONE);
+        contentPet.setVisibility(input ? GONE : View.VISIBLE);
+        return this;
+    }
+
+    public CommPop setContent(CharSequence s) {
+        contentPet.setText(s);
+        contentPtv.setText(s);
+        return this;
+    }
+
+    public CommPop setContentHint(CharSequence s) {
+        contentPet.setHint(s);
+        return this;
+    }
+
+    public CommPop setContentView(View view) {
+        RelativeLayout rl = findViewById(R.id.content_rl);
+        for (int i = 0; i < rl.getChildCount(); i++)
+            rl.getChildAt(i).setVisibility(GONE);
+        rl.addView(view);
+        return this;
+    }
+
+    public CommPop setCancelHide() {
+        cancelPtv.setVisibility(GONE);
+        return this;
+    }
+
+    public CommPop setShowExit() {
+        exitPiv.setVisibility(View.VISIBLE);
+        return this;
+    }
+
+    public CommPop setOutsideDismiss() {  // 逆默认值
+        setOutsideDismiss(true);
+        return this;
+    }
+
+    public CommPop setOutsideDismiss(Boolean dismiss) {
+        setAllowDismissWhenTouchOutside(dismiss);
+        return this;
+    }
+
+    public CommPop setBackPressDismiss() { // 逆默认值
+        setBackPressDismiss(true);
+        return this;
+    }
+
+    public CommPop setBackPressDismiss(Boolean dismiss) {
+        setBackPressEnable(dismiss);
+        return this;
+    }
+
+    public CommPop setCancelTxtColor(@ColorInt int c) {
+        cancelPtv.setTextColor(c);
+        return this;
+    }
+
+    public CommPop setConfirmTxtColor(@ColorInt int c) {
+        confirmPtv.setTextColor(c);
+        return this;
+    }
+
+    public CommPop setCancelLis(final OnCancelClickListener lis) {
+        autoCancel = false;
+        cancelPtv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lis.onCancel();
+            }
+        });
+        return this;
+    }
+
+    public CommPop setConfirmLis(final OnConfirmClickListener lis) {
+        confirmPtv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lis.onConfirm(contentPet.getContent());
+            }
+        });
+        return this;
+    }
+
+    public CommPop setHandleOj(OnHandleOjListener lis) {
+        lis.onHandle(this);
+        return this;
+    }
+
+    public CommPop setHandleView(OnHandleViewListener lis) {
+        lis.onHandle(getContentView());
+        return this;
+    }
+
+
+    public void show() {
+        setPopupGravity(Gravity.CENTER);
+        showPopupWindow();
+    }
+
+    ///////////////////////////
+
+    public interface OnConfirmClickListener {
+        void onConfirm(@Nullable String s);
+    }
+
+    public interface OnCancelClickListener {
+        void onCancel();
+    }
+
+    public interface OnHandleOjListener {
+        void onHandle(CommPop pop);
+    }
+
+    public interface OnHandleViewListener {
+        void onHandle(View view);
+    }
+}
