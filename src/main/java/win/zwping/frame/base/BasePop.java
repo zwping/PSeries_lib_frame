bb577100c5c715e6641742e00b1d73d19c2a305d
zwping <1101558280@qq.com>
2019-06-05 11:11:27 +0800

    debug ing

---
+++ src/main/java/win/zwping/frame/base/BasePop.java
@@ -0,0 +1,69 @@
+package win.zwping.frame.base;
+
+import android.content.Context;
+import android.view.View;
+import androidx.annotation.FloatRange;
+import androidx.annotation.IntDef;
+import androidx.annotation.IntRange;
+import com.chad.library.adapter.base.BaseViewHolder;
+import razerdp.basepopup.BasePopupWindow;
+import win.zwping.code.utils.ScreenUtil;
+
+public abstract class BasePop extends BasePopupWindow {
+
+    //////////////////////////////////////////////
+
+    public BaseViewHolder getViewHolder() {
+        return new BaseViewHolder(getContentView());
+    }
+
+    public void setOnDismiss(int... ids) {
+        for (int id : ids) {
+            findViewById(id).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+        }
+    }
+
+    public void setGones(int... ids) {
+        for (int id : ids) {
+            findViewById(id).setVisibility(View.GONE);
+        }
+    }
+
+    public void setVisibles(int... ids) {
+        for (int id : ids) {
+            findViewById(id).setVisibility(View.VISIBLE);
+        }
+    }
+
+    public static int getScreenWidth(float percentage) {
+        return Math.round(ScreenUtil.getScreenWidth() * percentage);
+    }
+
+    public static int getScreenHeight(float percentage) {
+        return Math.round(ScreenUtil.getScreenHeight() * percentage);
+    }
+
+    //////////////////////////////////////////////
+
+    public BasePop(Context context) {
+        super(context);
+    }
+
+    public BasePop(Context context, boolean delayInit) {
+        super(context, delayInit);
+    }
+
+    public BasePop(Context context, int width, int height) {
+        super(context, width, height);
+    }
+
+    public BasePop(Context context, int width, int height, boolean delayInit) {
+        super(context, width, height, delayInit);
+    }
+
+}
---
+++ src/main/java/win/zwping/frame/lis/OnPopupListener.java
@@ -0,0 +1,10 @@
+package win.zwping.frame.lis;
+
+import razerdp.basepopup.BasePopupWindow;
+import win.zwping.frame.base.BasePop;
+
+public interface OnPopupListener {
+
+    void onPop(BasePop it);
+
+}
