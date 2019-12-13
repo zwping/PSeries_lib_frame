bc6ccb1fd77ade6dcb66fdd00337decd14771d5f
zwping <1101558280@qq.com>
2019-12-13 17:34:25 +0800

    1

--- src/main/java/win/zwping/frame/mvvm/BaseVmAc.java
+++ src/main/java/win/zwping/frame/mvvm/BaseVmAc.java
@@ -8,6 +8,10 @@ import androidx.databinding.ViewDataBinding;
 
 import win.zwping.frame.base.BaseAc;
 
+/**
+ * 支持mvvm的ac基类，支持ktx混用
+ * @param <Bind>
+ */
 public abstract class BaseVmAc<Bind extends ViewDataBinding> extends BaseAc {
 
     protected Bind bind;
